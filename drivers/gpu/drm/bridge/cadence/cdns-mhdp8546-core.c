582b06acc3746f6181cb5cdf73ca6e340d658af6
Michael Walle <michael@walle.cc>
2021-06-05 16:18:50 +0200

    drm: bridge: cdns-mhdp8546: move cdns_mhdp_configure_video() to common part
    The configure_video() will be reused for the MHDP8501, move it to the
    common module.
    
    Signed-off-by: Michael Walle <michael@walle.cc>

--- drivers/gpu/drm/bridge/cadence/cdns-mhdp8546-core.c
+++ drivers/gpu/drm/bridge/cadence/cdns-mhdp8546-core.c
@@ -943,31 +943,6 @@ static int cdns_mhdp_connector_detect(struct drm_connector *conn,
 	return cdns_mhdp_detect(mhdp);
 }
 
-static u32 cdns_mhdp_get_bpp(struct cdns_mhdp_display_fmt *fmt)
-{
-	u32 bpp;
-
-	if (fmt->y_only)
-		return fmt->bpc;
-
-	switch (fmt->color_format) {
-	case DRM_COLOR_FORMAT_RGB444:
-	case DRM_COLOR_FORMAT_YCRCB444:
-		bpp = fmt->bpc * 3;
-		break;
-	case DRM_COLOR_FORMAT_YCRCB422:
-		bpp = fmt->bpc * 2;
-		break;
-	case DRM_COLOR_FORMAT_YCRCB420:
-		bpp = fmt->bpc * 3 / 2;
-		break;
-	default:
-		bpp = fmt->bpc * 3;
-		WARN_ON(1);
-	}
-	return bpp;
-}
-
 static
 bool cdns_mhdp_bandwidth_ok(struct cdns_mhdp_device *mhdp,
 			    const struct drm_display_mode *mode,
@@ -1147,184 +1122,6 @@ aux_unregister:
 	return ret;
 }
 
-static void cdns_mhdp_configure_video(struct cdns_mhdp_device *mhdp,
-				      const struct drm_display_mode *mode)
-{
-	unsigned int dp_framer_sp = 0, msa_horizontal_1,
-		msa_vertical_1, bnd_hsync2vsync, hsync2vsync_pol_ctrl,
-		misc0 = 0, misc1 = 0, pxl_repr,
-		front_porch, back_porch, msa_h0, msa_v0, hsync, vsync,
-		dp_vertical_1;
-	u8 stream_id = mhdp->stream_id;
-	u32 bpp, bpc, pxlfmt, framer;
-	int ret;
-
-	pxlfmt = mhdp->display_fmt.color_format;
-	bpc = mhdp->display_fmt.bpc;
-
-	/*
-	 * If YCBCR supported and stream not SD, use ITU709
-	 * Need to handle ITU version with YCBCR420 when supported
-	 */
-	if ((pxlfmt == DRM_COLOR_FORMAT_YCRCB444 ||
-	     pxlfmt == DRM_COLOR_FORMAT_YCRCB422) && mode->crtc_vdisplay >= 720)
-		misc0 = DP_YCBCR_COEFFICIENTS_ITU709;
-
-	bpp = cdns_mhdp_get_bpp(&mhdp->display_fmt);
-
-	switch (pxlfmt) {
-	case DRM_COLOR_FORMAT_RGB444:
-		pxl_repr = CDNS_DP_FRAMER_RGB << CDNS_DP_FRAMER_PXL_FORMAT;
-		misc0 |= DP_COLOR_FORMAT_RGB;
-		break;
-	case DRM_COLOR_FORMAT_YCRCB444:
-		pxl_repr = CDNS_DP_FRAMER_YCBCR444 << CDNS_DP_FRAMER_PXL_FORMAT;
-		misc0 |= DP_COLOR_FORMAT_YCbCr444 | DP_TEST_DYNAMIC_RANGE_CEA;
-		break;
-	case DRM_COLOR_FORMAT_YCRCB422:
-		pxl_repr = CDNS_DP_FRAMER_YCBCR422 << CDNS_DP_FRAMER_PXL_FORMAT;
-		misc0 |= DP_COLOR_FORMAT_YCbCr422 | DP_TEST_DYNAMIC_RANGE_CEA;
-		break;
-	case DRM_COLOR_FORMAT_YCRCB420:
-		pxl_repr = CDNS_DP_FRAMER_YCBCR420 << CDNS_DP_FRAMER_PXL_FORMAT;
-		break;
-	default:
-		pxl_repr = CDNS_DP_FRAMER_Y_ONLY << CDNS_DP_FRAMER_PXL_FORMAT;
-	}
-
-	switch (bpc) {
-	case 6:
-		misc0 |= DP_TEST_BIT_DEPTH_6;
-		pxl_repr |= CDNS_DP_FRAMER_6_BPC;
-		break;
-	case 8:
-		misc0 |= DP_TEST_BIT_DEPTH_8;
-		pxl_repr |= CDNS_DP_FRAMER_8_BPC;
-		break;
-	case 10:
-		misc0 |= DP_TEST_BIT_DEPTH_10;
-		pxl_repr |= CDNS_DP_FRAMER_10_BPC;
-		break;
-	case 12:
-		misc0 |= DP_TEST_BIT_DEPTH_12;
-		pxl_repr |= CDNS_DP_FRAMER_12_BPC;
-		break;
-	case 16:
-		misc0 |= DP_TEST_BIT_DEPTH_16;
-		pxl_repr |= CDNS_DP_FRAMER_16_BPC;
-		break;
-	}
-
-	bnd_hsync2vsync = CDNS_IP_BYPASS_V_INTERFACE;
-	if (mode->flags & DRM_MODE_FLAG_INTERLACE)
-		bnd_hsync2vsync |= CDNS_IP_DET_INTERLACE_FORMAT;
-
-	cdns_mhdp_reg_write(&mhdp->mbox, CDNS_BND_HSYNC2VSYNC(stream_id),
-			    bnd_hsync2vsync);
-
-	hsync2vsync_pol_ctrl = 0;
-	if (mode->flags & DRM_MODE_FLAG_NHSYNC)
-		hsync2vsync_pol_ctrl |= CDNS_H2V_HSYNC_POL_ACTIVE_LOW;
-	if (mode->flags & DRM_MODE_FLAG_NVSYNC)
-		hsync2vsync_pol_ctrl |= CDNS_H2V_VSYNC_POL_ACTIVE_LOW;
-	cdns_mhdp_reg_write(&mhdp->mbox, CDNS_HSYNC2VSYNC_POL_CTRL(stream_id),
-			    hsync2vsync_pol_ctrl);
-
-	cdns_mhdp_reg_write(&mhdp->mbox,
-			    CDNS_DP_MST_FRAMER_PXL_REPR(stream_id), pxl_repr);
-
-	if (mode->flags & DRM_MODE_FLAG_INTERLACE)
-		dp_framer_sp |= CDNS_DP_FRAMER_INTERLACE;
-	if (mode->flags & DRM_MODE_FLAG_NHSYNC)
-		dp_framer_sp |= CDNS_DP_FRAMER_HSYNC_POL_LOW;
-	if (mode->flags & DRM_MODE_FLAG_NVSYNC)
-		dp_framer_sp |= CDNS_DP_FRAMER_VSYNC_POL_LOW;
-	cdns_mhdp_reg_write(&mhdp->mbox, CDNS_DP_MST_FRAMER_SP(stream_id),
-			    dp_framer_sp);
-
-	front_porch = mode->crtc_hsync_start - mode->crtc_hdisplay;
-	back_porch = mode->crtc_htotal - mode->crtc_hsync_end;
-	cdns_mhdp_reg_write(&mhdp->mbox,
-			    CDNS_DP_MST_FRONT_BACK_PORCH(stream_id),
-			    CDNS_DP_FRONT_PORCH(front_porch) |
-			    CDNS_DP_BACK_PORCH(back_porch));
-
-	cdns_mhdp_reg_write(&mhdp->mbox, CDNS_DP_MST_BYTE_COUNT(stream_id),
-			    mode->crtc_hdisplay * bpp / 8);
-
-	msa_h0 = mode->crtc_htotal - mode->crtc_hsync_start;
-	cdns_mhdp_reg_write(&mhdp->mbox,
-			    CDNS_DP_MST_MSA_HORIZONTAL_0(stream_id),
-			    CDNS_DP_MSAH0_H_TOTAL(mode->crtc_htotal) |
-			    CDNS_DP_MSAH0_HSYNC_START(msa_h0));
-
-	hsync = mode->crtc_hsync_end - mode->crtc_hsync_start;
-	msa_horizontal_1 = CDNS_DP_MSAH1_HSYNC_WIDTH(hsync) |
-			   CDNS_DP_MSAH1_HDISP_WIDTH(mode->crtc_hdisplay);
-	if (mode->flags & DRM_MODE_FLAG_NHSYNC)
-		msa_horizontal_1 |= CDNS_DP_MSAH1_HSYNC_POL_LOW;
-	cdns_mhdp_reg_write(&mhdp->mbox,
-			    CDNS_DP_MST_MSA_HORIZONTAL_1(stream_id),
-			    msa_horizontal_1);
-
-	msa_v0 = mode->crtc_vtotal - mode->crtc_vsync_start;
-	cdns_mhdp_reg_write(&mhdp->mbox, CDNS_DP_MST_MSA_VERTICAL_0(stream_id),
-			    CDNS_DP_MSAV0_V_TOTAL(mode->crtc_vtotal) |
-			    CDNS_DP_MSAV0_VSYNC_START(msa_v0));
-
-	vsync = mode->crtc_vsync_end - mode->crtc_vsync_start;
-	msa_vertical_1 = CDNS_DP_MSAV1_VSYNC_WIDTH(vsync) |
-			 CDNS_DP_MSAV1_VDISP_WIDTH(mode->crtc_vdisplay);
-	if (mode->flags & DRM_MODE_FLAG_NVSYNC)
-		msa_vertical_1 |= CDNS_DP_MSAV1_VSYNC_POL_LOW;
-	cdns_mhdp_reg_write(&mhdp->mbox, CDNS_DP_MST_MSA_VERTICAL_1(stream_id),
-			    msa_vertical_1);
-
-	if ((mode->flags & DRM_MODE_FLAG_INTERLACE) &&
-	    mode->crtc_vtotal % 2 == 0)
-		misc1 = DP_TEST_INTERLACED;
-	if (mhdp->display_fmt.y_only)
-		misc1 |= CDNS_DP_TEST_COLOR_FORMAT_RAW_Y_ONLY;
-	/* Use VSC SDP for Y420 */
-	if (pxlfmt == DRM_COLOR_FORMAT_YCRCB420)
-		misc1 = CDNS_DP_TEST_VSC_SDP;
-
-	cdns_mhdp_reg_write(&mhdp->mbox, CDNS_DP_MST_MSA_MISC(stream_id),
-			    misc0 | (misc1 << 8));
-
-	cdns_mhdp_reg_write(&mhdp->mbox, CDNS_DP_MST_HORIZONTAL(stream_id),
-			    CDNS_DP_H_HSYNC_WIDTH(hsync) |
-			    CDNS_DP_H_H_TOTAL(mode->crtc_hdisplay));
-
-	cdns_mhdp_reg_write(&mhdp->mbox, CDNS_DP_MST_VERTICAL_0(stream_id),
-			    CDNS_DP_V0_VHEIGHT(mode->crtc_vdisplay) |
-			    CDNS_DP_V0_VSTART(msa_v0));
-
-	dp_vertical_1 = CDNS_DP_V1_VTOTAL(mode->crtc_vtotal);
-	if ((mode->flags & DRM_MODE_FLAG_INTERLACE) &&
-	    mode->crtc_vtotal % 2 == 0)
-		dp_vertical_1 |= CDNS_DP_V1_VTOTAL_EVEN;
-
-	cdns_mhdp_reg_write(&mhdp->mbox, CDNS_DP_MST_VERTICAL_1(stream_id),
-			    dp_vertical_1);
-
-	cdns_mhdp_reg_write_bit(&mhdp->mbox, CDNS_DP_MST_VB_ID(stream_id), 2,
-				1, (mode->flags & DRM_MODE_FLAG_INTERLACE) ?
-				CDNS_DP_VB_ID_INTERLACED : 0);
-
-	ret = cdns_mhdp_reg_read(&mhdp->mbox, CDNS_DP_FRAMER_GLOBAL_CONFIG,
-				 &framer);
-	if (ret < 0) {
-		dev_err(mhdp->dev,
-			"Failed to read CDNS_DP_FRAMER_GLOBAL_CONFIG %d\n",
-			ret);
-		return;
-	}
-	framer |= CDNS_DP_FRAMER_EN;
-	framer &= ~CDNS_DP_NO_VIDEO_MODE;
-	cdns_mhdp_reg_write(&mhdp->mbox, CDNS_DP_FRAMER_GLOBAL_CONFIG, framer);
-}
-
 static void cdns_mhdp_sst_enable(struct cdns_mhdp_device *mhdp,
 				 const struct drm_display_mode *mode)
 {
@@ -1367,7 +1164,8 @@ static void cdns_mhdp_sst_enable(struct cdns_mhdp_device *mhdp,
 			    CDNS_DP_SC2_TU_VS_DIFF((tu_size - vs > 3) ?
 						   0 : tu_size - vs));
 
-	cdns_mhdp_configure_video(mhdp, mode);
+	cdns_mhdp_configure_video(&mhdp->mbox, mhdp->stream_id,
+				  &mhdp->display_fmt, mode);
 }
 
 static void cdns_mhdp_atomic_enable(struct drm_bridge *bridge,
