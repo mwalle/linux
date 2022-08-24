e4e9fec525ece67faa68e4da4700c87ba30897b7
Michael Walle <michael@walle.cc>
2022-08-24 19:13:09 +0200

    mtd: core: fix unique name (TODO!)
    Signed-off-by: Michael Walle <michael@walle.cc>

--- drivers/mtd/mtdcore.c
+++ drivers/mtd/mtdcore.c
@@ -880,8 +880,7 @@ static struct nvmem_device *mtd_otp_nvmem_register(struct mtd_info *mtd,
 
 	/* OTP nvmem will be registered on the physical device */
 	config.dev = mtd->dev.parent;
-	config.name = kasprintf(GFP_KERNEL, "%s-%s", dev_name(&mtd->dev), compatible);
-	config.id = NVMEM_DEVID_NONE;
+	config.name = compatible;
 	config.owner = THIS_MODULE;
 	config.type = NVMEM_TYPE_OTP;
 	config.root_only = true;
@@ -897,7 +896,6 @@ static struct nvmem_device *mtd_otp_nvmem_register(struct mtd_info *mtd,
 		nvmem = NULL;
 
 	of_node_put(np);
-	kfree(config.name);
 
 	return nvmem;
 }
