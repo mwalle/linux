ffb9ebbec3e86b0c9fbae034b123769f659de732
Stephen Rothwell <sfr@canb.auug.org.au>
2022-09-20 17:13:29 +1000

    Revert "lib/cpumask: add FORCE_NR_CPUS config option"
    This reverts commit c90c5bd9f2d6d43b3e295bb918bb6bcc4b0eaf91.

--- kernel/smp.c
+++ kernel/smp.c
@@ -1087,7 +1087,7 @@ static int __init maxcpus(char *str)
 
 early_param("maxcpus", maxcpus);
 
-#if (NR_CPUS > 1) && !defined(CONFIG_FORCE_NR_CPUS)
+#if (NR_CPUS > 1)
 /* Setup number of possible processor ids */
 unsigned int nr_cpu_ids __read_mostly = NR_CPUS;
 EXPORT_SYMBOL(nr_cpu_ids);
